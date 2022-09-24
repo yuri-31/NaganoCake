249104b0d328a103d07a14e83ed48f4c30eca918
YuriNakano <yuriinlethbridge@outlook.com>
2022-09-24 09:33:40 +0000

    [Create] Cart_itemモデル/cart_itemsテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_24_092545) do
+ActiveRecord::Schema.define(version: 2022_09_24_092907) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,14 @@ ActiveRecord::Schema.define(version: 2022_09_24_092545) do
     t.string "name", null: false
     t.string "postal_code", null: false
     t.string "adress", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "customer_id", null: false
+    t.integer "amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
