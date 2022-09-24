216c4dd5a4e46a771498ca72d9765d0783a5af21
YuriNakano <yuriinlethbridge@outlook.com>
2022-09-24 06:09:12 +0000

    [Add] devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
