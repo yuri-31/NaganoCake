6b1e07bc8d3d2ce0698ce2a2e4bd59363edd4157
YuriNakano <yuriinlethbridge@outlook.com>
2022-09-27 06:01:04 +0000

    [Modified] 管理者/顧客のdevise設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,39 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'adresses/index'
-    get 'adresses/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
+  
+  root to: 'public/homes#top'
+  get 'home/about' => 'public/homes#about', as: 'about'
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: 'admin/sessions',
+  }
+  
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
   namespace :admin do
-    get 'orders/show'
-    get 'orders/index'
+    resources :confirmations, only: [:new]
+    resources :customers, only: [:index, :show, :edit]
+    resources :genres, only: [:index, :edit]
+    resources :homes, only: [:top]
+    resources :items, only: [:index, :new, :show, :edit]
+    resources :orders, only: [:show, :index]
+    resources :sessions, only: [:new]
+    resources :unlocks, only: [:new]
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'sessions/new'
-  end
+  
   namespace :public do
-    get 'sessions/new'
-  end
-  namespace :public do
-    get 'registrations/new'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  devise_for :admins
-  devise_for :customers
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+    resources :adresses, only: [:index, :edit]
+    resources :cart_items, only: [:index]
+    resources :confirmations, only: [:new]
+    resources :customers, only: [:show, :edit]
+    resources :homes, only: [:about, :top]
+    resources :items, only: [:index, :show]
+    resources :orders, only: [:new, :index, :show]
+    resources :registrations, only: [:edit, :new]
+    resources :sessions, only: [:new]
+    resources :unlocks, only: [:new]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
