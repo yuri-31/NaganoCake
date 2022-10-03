abbd6d6463a54afe0a9282bc1a33ba48b8f165eb
YuriNakano <yuriinlethbridge@outlook.com>
2022-10-03 19:24:13 +0000

    [Finish] 会員一覧画面

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def full_name
+    customer.last_name + customer.first_name
+  end
 end
