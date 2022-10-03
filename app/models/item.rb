f2d5a9ec8ebd22856e5dd717616044d33f8de688
YuriNakano <yuriinlethbridge@outlook.com>
2022-10-03 09:30:24 +0000

    [Edit] ジャンル編集/商品新規登録/商品一覧画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+    
+    has_one_attached :item_image
+    
+    validates :name, presence: true
+    validates :introduction, presence: true
+    validates :price, presence: true
+    
 end
